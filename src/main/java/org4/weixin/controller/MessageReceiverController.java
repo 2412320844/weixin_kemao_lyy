31aefe5697062830bb1cb1a533903d48712a7112
LYY <LYY@liyuanyang>
2019-04-02 18:45:47 +0800

    接受微信公众号平台发送给我们的信息

--- src/main/java/org4/weixin/controller/MessageReceiverController.java
+++ src/main/java/org4/weixin/controller/MessageReceiverController.java
@@ -1,22 +1,37 @@
 package org4.weixin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/lyy/weixin/receiver")
 public class MessageReceiverController {
+	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
 	
 	@GetMapping
 	public String echo(
-			String signature,
-			String timestamp,
-			String nonce,
-			String echostr
+			@RequestParam("signature")String signature,
+			@RequestParam("timestamp")String timestamp,
+			@RequestParam("nonce")String nonce,
+			@RequestParam("echostr")String echostr
 			) {
 		
 		return echostr;
+	}
+	@PostMapping
+	public String onMessage(
+			@RequestParam("signature")String signature,
+			@RequestParam("timestamp")String timestamp,
+			@RequestParam("nonce")String nonce,
+			@RequestBody String xml) {
+		LOG.trace("收到的消息原文：\n{}\n------------------", xml);
+		return "success";
 	}
 }
